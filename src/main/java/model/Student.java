4e8666e6c24c14c46eb8c6f92e9bcb86d04ab84d
dbc2201 <divyanshb22@gmail.com>
2022-02-03 22:51:46 +0530

    add fields for Student class

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -2,6 +2,9 @@ package model;
 
 public class Student extends Person {
 
+    private String subject;
+    private String rollNumber;
+
     public Student(String firstName, String lastName, String emailAddress) {
         super(firstName, lastName, emailAddress);
     }
