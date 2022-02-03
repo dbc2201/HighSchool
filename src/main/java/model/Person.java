e4688d6ee46f238cef01fd461caef52517c292b1
dbc2201 <divyanshb22@gmail.com>
2022-02-03 22:37:33 +0530

    add equals() and hashCode() for the Person class

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Person {
     private String firstName;
     private String lastName;
@@ -33,5 +35,18 @@ public class Person {
 
     public void setEmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(emailAddress, person.emailAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, emailAddress);
     }
 }
