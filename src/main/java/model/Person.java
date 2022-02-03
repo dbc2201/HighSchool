0bfc9ee9e166e4e9746c4b4d2606e979cb8620af
dbc2201 <divyanshb22@gmail.com>
2022-02-03 22:35:57 +0530

    add constructor for the Person class

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -4,4 +4,10 @@ public class Person {
     private String firstName;
     private String lastName;
     private String emailAddress;
+
+    public Person(String firstName, String lastName, String emailAddress) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
 }
