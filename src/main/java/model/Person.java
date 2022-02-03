f8b149e43fbff4e9e01af2d2db2f9560b905a803
dbc2201 <divyanshb22@gmail.com>
2022-02-03 22:36:27 +0530

    add getters and setters for the Person class

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -10,4 +10,28 @@ public class Person {
         this.lastName = lastName;
         this.emailAddress = emailAddress;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
 }
