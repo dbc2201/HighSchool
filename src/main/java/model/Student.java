294c765d11147767747ad4e22bdb0091f64244a1
dbc2201 <divyanshb22@gmail.com>
2022-02-03 22:52:08 +0530

    add getters and setters for Student class

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -8,4 +8,20 @@ public class Student extends Person {
     public Student(String firstName, String lastName, String emailAddress) {
         super(firstName, lastName, emailAddress);
     }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
 }
