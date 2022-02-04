b78bd268c8ea8351041d5da0a0eef201693f85a5
dbc2201 <divyanshb22@gmail.com>
2022-02-04 16:33:17 +0530

    create a Person object and print its details

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,13 @@
 package main;
 
+import model.Person;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Person person = new Person("Divyansh", "Bhardwaj", "divyanshb22@gmail.com");
+        System.out.println("person = " + person);
+        JOptionPane.showMessageDialog(null, person, "Person Details", JOptionPane.PLAIN_MESSAGE);
     }
 }
