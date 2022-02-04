afe82877b33e67c33e0fad38c7fd463ac0032dd6
dbc2201 <divyanshb22@gmail.com>
2022-02-04 16:34:22 +0530

    create a Student object and print its details

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,7 @@
 package main;
 
 import model.Person;
+import model.Student;
 
 import javax.swing.*;
 
@@ -9,5 +10,9 @@ public class Main {
         Person person = new Person("Divyansh", "Bhardwaj", "divyanshb22@gmail.com");
         System.out.println("person = " + person);
         JOptionPane.showMessageDialog(null, person, "Person Details", JOptionPane.PLAIN_MESSAGE);
+
+        Student student = new Student("divyansh", "bhardwaj", "divyanshb22@gmail.com");
+        System.out.println("student = " + student);
+        JOptionPane.showMessageDialog(null, student, "Student Details", JOptionPane.PLAIN_MESSAGE);
     }
 }
