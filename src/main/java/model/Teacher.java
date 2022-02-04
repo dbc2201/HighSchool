31d6bbd7efb18a39fbd2a525dabd553928f79232
dbc2201 <divyanshb22@gmail.com>
2022-02-04 16:27:33 +0530

    add the getter and setter methods for the Teacher class

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -10,4 +10,20 @@ public class Teacher extends Person {
         this.salary = salary;
         this.subject = subject;
     }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
