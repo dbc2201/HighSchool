074db028e3c207b8df014b6b522512f12760fb77
dbc2201 <divyanshb22@gmail.com>
2022-02-04 16:27:04 +0530

    initialize the salary and subject fields for the Teacher class in its constructor

--- src/main/java/model/Teacher.java
+++ src/main/java/model/Teacher.java
@@ -5,7 +5,9 @@ public class Teacher extends Person {
     private double salary;
     private String subject;
 
-    public Teacher(String firstName, String lastName, String emailAddress) {
+    public Teacher(String firstName, String lastName, String emailAddress, double salary, String subject) {
         super(firstName, lastName, emailAddress);
+        this.salary = salary;
+        this.subject = subject;
     }
 }
