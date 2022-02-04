814fb22dcdc69d5bca462270f75d86d1612bf95e
dbc2201 <divyanshb22@gmail.com>
2022-02-04 16:30:20 +0530

    initialize the extra fields of the CollegeStudent model in its constructor

--- src/main/java/model/CollegeStudent.java
+++ src/main/java/model/CollegeStudent.java
@@ -5,7 +5,9 @@ public class CollegeStudent extends Student {
     private int currentYear;
     private String major;
 
-    public CollegeStudent(String firstName, String lastName, String emailAddress) {
+    public CollegeStudent(String firstName, String lastName, String emailAddress, int currentYear, String major) {
         super(firstName, lastName, emailAddress);
+        this.currentYear = currentYear;
+        this.major = major;
     }
 }
